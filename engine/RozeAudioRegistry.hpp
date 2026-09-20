0da5f661114849a074a6f31b383605a205a8e817
0x11semprez <traoresemprez@icloud.com>
2026-09-20 15:34:04 +0200

    feat: create all the format that roze will supports

---
+++ engine/RozeAudioRegistry.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <array>
+#include <string_view>
+
+using namespace std;
+
+class RozeAudioRegistry {
+    public:
+    static inline constexpr array<string_view, 47> allFormats{
+        ".3gp",  ".aa",     ".aac", ".aax",  ".act",  ".aiff",  ".alac", ".amr", ".ape", ".au",
+        ".awb",  ".dss",    ".dvf", ".flac", ".gsm",  ".iklax", ".ivs",  ".m4a", ".m4b", ".m4p",
+        ".mmf",  ".movpkg", ".mp1", ".mp2",  ".mp3",  ".mpc",   ".msv",  ".nmf", ".ogg", ".oga",
+        ".mogg", ".opus",   ".qoa", ".ra",   ".rm",   ".raw",   ".rf64", ".sln", ".tta", ".voc",
+        ".vox",  ".wav",    ".wma", ".wv",   ".webm", ".8svx",  ".cda"};
+};
