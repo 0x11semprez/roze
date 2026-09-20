c7af6c9dd9e81198f1d0c06266fd9c1a633f88d3
0x11semprez <traoresemprez@icloud.com>
2026-09-20 16:02:11 +0200

    feat: redefinition of the function errorMessage

---
+++ engine/errors/RozeAudioRegistryErrors.cpp
@@ -0,0 +1,13 @@
+#include "RozeAudioRegistryErrors.hpp"
+
+#include <string_view>
+
+string_view errorMessage(RozeAudioRegistryErrors error)
+{
+    switch (error) {
+        case RozeAudioRegistryErrors::FormatIsNotSupported:
+            return "roze loves you, This format is not supported by roze";
+        default:
+            return "roze don't understand what you trying to create";
+    }
+}
